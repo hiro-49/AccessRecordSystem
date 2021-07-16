81e2d92d2fb8a3d3396b5e6edc2557aa7d20eaef
EC2 Default User <ec2-user@ip-172-31-46-207.us-east-2.compute.internal>
2021-07-16 04:26:59 +0000

    Generate Members Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'members/new'
     root 'static_pages#home'
-    get  '/help', to: 'static_pages#help'
+    get '/help', to: 'static_pages#help'
+    get '/members', to: 'members#new';
 end
