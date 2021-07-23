b1ab35828b730e4d12431244ef855dc88ae34566
EC2 Default User <ec2-user@ip-172-31-46-207.us-east-2.compute.internal>
2021-07-23 03:50:57 +0000

    Bug fixing

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,8 +5,8 @@ class RecordsController < ApplicationController
   end
   
   def access
-    idm = params(:idm)
-    temperature = params(:temperature).to_f
+    idm = params[:idm]
+    temperature = params[:temperature].to_f
     if(@member = Member.find_by(idm: idm))
       @record = Record.new(name: @member.name, number: @member.number, temperature: temperature)
       @record.save
